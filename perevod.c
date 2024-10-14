1874a78f62037da03a05375ee3188e94f8e3279c
vadimpopov1 <bonia07052006@icloud.com>
2024-10-14 14:09:04 +0700

    perevod.c

--- perevod.c
+++ perevod.c
@@ -18,13 +18,13 @@ int fone(int a) {
     return pos;
 } 
 
-int shift(unsigned int a, unsigned int k) {
-    unsigned int size = sizeof(int);
-    k %= size;
-    unsigned int r_shift = a >> k;
-    unsigned int l_shift = a << (size - k);
-    return r_shift | l_shift;
-}
+//int shift(unsigned int a, unsigned int k) {
+//    unsigned int size = sizeof(int);
+//    k %= size;
+//    unsigned int r_shift = a >> k;
+//    unsigned int l_shift = a << (size - k);
+//    return r_shift | l_shift;
+//}
 
 int main() {
     unsigned int a;
@@ -35,8 +35,8 @@ int main() {
     scanf("%u", &k);
     int fzero_bit = fzero(a);
     int fone_bit = fone(a)/2;
-    unsigned int shift_bit = shift(a,k);
+//    unsigned int shift_bit = shift(a,k);
     printf("Last zero bit: %d \n", fzero_bit + 1);
     printf("Last one bit: %d \n", fone_bit + 1);
-    printf("Shifted: %d \n", shift_bit);
+//    printf("Shifted: %d \n", shift_bit);
 }
